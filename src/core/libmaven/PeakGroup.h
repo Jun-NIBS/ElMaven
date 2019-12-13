a3b56c639bbcae32a3a3275d013f3fb4484c7a90
Saiful B. Khan <saiful.khan@elucidata.io>
2019-12-13 10:34:00 +0530

    Add SHAP-style classification inference

--- src/core/libmaven/PeakGroup.h
+++ src/core/libmaven/PeakGroup.h
@@ -153,7 +153,7 @@ class PeakGroup{
 
         ClassifiedLabel predictedLabel;
         float predictionProbability;
-        map<string, float> predictionInference;
+        multimap<float, string> predictionInference;
 
         double minQuality;
         float maxPeakFracionalArea;
